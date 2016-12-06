133476d9fed6fa9863add915c27da391eac28ea8
jrdbnntt <jrdbnntt@gmail.com>
2016-12-06 01:00:26 -0500

    only enable some setting in production, such as csrf protection

--- hackfsu_com/settings.py
+++ hackfsu_com/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -69,7 +68,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-
                 'django.template.context_processors.request',           # allauth
             ],
         },
@@ -134,3 +132,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'website/static'),
     os.path.join(BASE_DIR, 'website/static_build'),
 ]
+
+
+# Add more strict rules in production
+if not DEBUG:
+    MIDDLEWARE.extend([
+        'django.middleware.csrf.CsrfViewMiddleware',
+    ])

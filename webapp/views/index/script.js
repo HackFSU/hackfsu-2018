9ea6d20ec4783c5431cb35ddeb7f3f2dc052e23a
jrdbnntt <jrdbnntt@gmail.com>
2017-01-11 00:12:00 -0500

    index - dynamic faq complete

--- webapp/views/index/script.js
+++ webapp/views/index/script.js
@@ -1,4 +1,13 @@
 (function() {
     'use strict';
 
+    /**
+     * FAQ Text Toggling
+     */
+    var faqs = $('.faq-item-box');
+    var activeClass = 'active';
+    faqs.on('click', function() {
+        faqs.filter('.' + activeClass).not(this).removeClass(activeClass);
+        $(this).toggleClass(activeClass);
+    });
 })();

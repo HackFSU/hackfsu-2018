f3ebe1fd2885ebc36ffc0688fa156cd786b3ef80
jrdbnntt <jrdbnntt@gmail.com>
2017-01-13 17:02:48 -0500

    fixed wsgi path with default paths

--- hackfsu_com/wsgi.py
+++ hackfsu_com/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackfsu_com.settings')
+os.environ.setdefault('PATH_PROJECT', os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.environ.setdefault('PATH_ENVIRONMENT_PACKAGES', os.path.join(
+    os.environ.get('PATH_PROJECT'), './venv/lib/python3.5/site-packages'))
+
+sys.path.append(os.environ.get('PATH_PROJECT'))
+sys.path.append(os.environ.get('PATH_ENVIRONMENT_PACKAGES'))
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackfsu_com.settings")
 
 application = get_wsgi_application()

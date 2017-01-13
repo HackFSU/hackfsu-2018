87e7435f83374caf05cdcaae736f21aadafdbd3e
jrdbnntt <jrdbnntt@gmail.com>
2017-01-12 19:35:22 -0500

    static root added for collect static

--- hackfsu_com/settings.py
+++ hackfsu_com/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = '/var/www/hackfsu_com/static_collected/'
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'webapp/build/static'),

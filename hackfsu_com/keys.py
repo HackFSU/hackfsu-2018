e42df6b94a9dfaba1e163d4417ccdf4a08f28a90
jrdbnntt <jrdbnntt@gmail.com>
2017-01-19 08:12:17 -0500

    mandrill email sending utility for transactional emails

--- hackfsu_com/keys.py
+++ hackfsu_com/keys.py
@@ -16,6 +16,7 @@ DB_PORT = os.getenv('DB_PORT')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 RECAPTCHA_SECRET = os.getenv('RECAPTCHA_SECRET')
+MANDRILL_API_KEY = os.getenv('MANDRILL_API_KEY')
 
 
 def load_secret_keys():
@@ -27,6 +28,7 @@ def load_secret_keys():
     global DB_USER
     global DB_PASSWORD
     global RECAPTCHA_SECRET
+    global MANDRILL_API_KEY
 
     with open(os.path.dirname(os.path.abspath(__file__)) + '/secret_keys.json') as file:
         secret_keys = json.load(file)
@@ -44,6 +46,7 @@ def load_secret_keys():
         DB_USER = set_if_exists(DB_USER, 'DB_USER')
         DB_PASSWORD = set_if_exists(DB_PASSWORD, 'DB_PASSWORD')
         RECAPTCHA_SECRET = set_if_exists(RECAPTCHA_SECRET, 'RECAPTCHA_SECRET')
+        MANDRILL_API_KEY = set_if_exists(MANDRILL_API_KEY, 'MANDRILL_API_KEY')
 
 
 load_secret_keys()

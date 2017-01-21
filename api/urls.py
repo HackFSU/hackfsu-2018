cd51b92327800189636e3426802dcaaab09020c6
jrdbnntt <jrdbnntt@gmail.com>
2017-01-21 16:33:26 -0500

    disable api user login and register for now

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,9 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    url(r'user/login$', views.user.LogInView.as_view(), name='user-login'),
+    # url(r'user/login$', views.user.LogInView.as_view(), name='user-login'),
     url(r'user/get/profile$', views.user.ProfileView.as_view(), name='user-get-profile'),
-    url(r'user/register$', views.user.RegisterView.as_view(), name='user-register'),
+    # url(r'user/register$', views.user.RegisterView.as_view(), name='user-register'),
 
     url(r'hacker/register$', views.hacker.RegisterView.as_view(), name='hacker-register'),
 

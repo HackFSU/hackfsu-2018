700c616315f5b01587b4e447f07a9530eb562b72
jrdbnntt <jrdbnntt@gmail.com>
2017-01-21 17:48:32 -0500

    wsgi fix

--- hackfsu_com/wsgi.py
+++ hackfsu_com/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import traceback
+import signal
 import sys
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackfsu_com.settings')
@@ -20,4 +22,10 @@ sys.path.append(os.environ.get('PATH_ENVIRONMENT_PACKAGES'))
 
 from django.core.wsgi import get_wsgi_application
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception as e:
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)

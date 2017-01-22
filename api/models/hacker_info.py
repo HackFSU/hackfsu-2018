0ddd4a0c28a07c593f12a7ab152e9a89be61b7f8
jrdbnntt <jrdbnntt@gmail.com>
2017-01-22 12:30:09 -0500

    updated hacker_info model to include interests and more school_year options

--- api/models/hacker_info.py
+++ api/models/hacker_info.py
@@ -27,6 +27,7 @@ class HackerInfo(models.Model):
     school_year = models.CharField(max_length=2, choices=SCHOOL_YEAR_CHOICES)
     school_major = models.CharField(max_length=100)
     resume_file_name = models.CharField(max_length=300, default='', blank=True)
+    interests = models.CharField(max_length=500, default='', blank=True)
 
     rsvp = models.BooleanField(default=False, blank=True)
     checked_in = models.BooleanField(default=False, blank=True)

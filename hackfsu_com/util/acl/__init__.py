d6521f7d04e2048bffc7dbd2b981e75cbb52966a
jrdbnntt <jrdbnntt@gmail.com>
2017-01-24 22:06:12 -0500

    updated acl group utility functions to handle multiple groups + be more efficient

--- hackfsu_com/util/acl/__init__.py
+++ hackfsu_com/util/acl/__init__.py
@@ -16,30 +16,68 @@ group_pending_mentor = 'pending-mentor'
 group_pending_judge = 'pending-judge'
 group_pending_organizer = 'pending-organizer'
 
-
-groups = [
-    group_user,
+db_groups = [
     group_hacker,
     group_mentor,
     group_judge,
     group_organizer,
-    group_admin,
     group_pending_hacker,
     group_pending_mentor,
     group_pending_judge,
     group_pending_organizer
 ]
 
+groups = [
+    group_user,
+    group_admin,
+]
+groups.extend(db_groups)
+
+
+def validate_db_group(group_name: str):
+    if group_name not in db_groups:
+        raise ValueError('Group "{}" is not a database group'.format(group_name))
+
+
+def validate_db_groups(group_names: list):
+    for name in group_names:
+        validate_db_group(name)
+
 
 def add_user_to_group(user: User, group_name: str):
-    group = Group.objects.get(name=group_name)
-    if not user.groups.filter(name=group_name).exists():
+    """ Adds user to a single group if not already in it """
+    validate_db_group(group_name)
+    group_to_add = Group.objects.get(name=group_name)
+    if not user.groups.filter(name=group_to_add).exists():
         user.groups.add(group)
     user.save()
 
 
 def remove_user_from_group(user: User, group_name: str):
-    group = Group.objects.get(name=group_name)
-    if user.groups.filter(name=group_name).exists():
-        user.groups.remove(group)
+    """ Removes user from a single group if in it """
+    validate_db_group(group_name)
+    matched_group = user.groups.filter(name=group_name)
+    if matched_group.exists():
+        user.groups.remove(matched_group)
+    user.save()
+
+
+def add_user_to_groups(user: User, group_names: list):
+    """ Adds user to multiple groups if not already in them """
+    validate_db_groups(group_names)
+    groups_to_add = Group.objects.filter(name__in=[group_names])
+    if groups_to_add.exists():
+        for group in groups_to_add:
+            if not user.group.filter(id=group.id).exists():
+                user.groups.add(group)
+    user.save()
+
+
+def remove_user_from_groups(user: User, group_names: list):
+    """ Removes user from multiple groups if in them """
+    validate_db_groups(group_names)
+    matched_groups = user.groups.filter(name__in=group_names)
+    if matched_groups.exists():
+        for group in matched_groups:
+            user.groups.remove(group)
     user.save()

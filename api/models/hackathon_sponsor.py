5c2e8b95639a98f6b4be32a45a9328543b26ee23
jrdbnntt <jrdbnntt@gmail.com>
2017-01-28 16:35:36 -0500

    db migration, sponsor tier now is choice-only

--- api/models/hackathon_sponsor.py
+++ api/models/hackathon_sponsor.py
@@ -3,11 +3,20 @@ from api.models import Hackathon
 
 
 class HackathonSponsor(models.Model):
+
+    SPONSOR_TIERS = (
+        (0, 'Co-Host'),
+        (1, 'Tier 3'),
+        (2, 'Tier 2'),
+        (3, 'Tier 1'),
+        (4, 'Partner')
+    )
+
     hackathon = models.ForeignKey(to=Hackathon, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     website_link = models.CharField(max_length=500)
     logo_link = models.CharField(max_length=500)
-    tier = models.SmallIntegerField(default=0)
+    tier = models.SmallIntegerField(default=0, choices=SPONSOR_TIERS)
     order = models.SmallIntegerField(default=0)
 
     def __str__(self):

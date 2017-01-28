babb36da5d6e42d65cadaad096f91fa361b65733
jrdbnntt <jrdbnntt@gmail.com>
2017-01-28 17:55:16 -0500

    db migration for schedule_item updates; end and descrion now optional; schedule_item advanced admin

--- api/models/schedule_item.py
+++ api/models/schedule_item.py
@@ -1,10 +1,21 @@
 from django.db import models
+from django.contrib import admin
 from api.models import Hackathon
 
 
 class ScheduleItem(models.Model):
     hackathon = models.ForeignKey(to=Hackathon, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
+    description = models.CharField(max_length=500, default='', blank=True)
     start = models.DateTimeField()
-    end = models.DateTimeField()
+    end = models.DateTimeField(null=True, blank=True)
+
+
+@admin.register(ScheduleItem)
+class ScheduleItemAdmin(admin.ModelAdmin):
+    list_filter = ('hackathon',)
+    list_display = ('hackathon', 'name',  'start', 'end', 'description')
+    list_editable = ('hackathon', 'start', 'end')
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    ordering = ('hackathon', 'start')

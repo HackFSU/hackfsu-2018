b73e4ae9098e4f7871075c209c7cfb405fb1cde3
jrdbnntt <jrdbnntt@gmail.com>
2017-01-28 18:34:34 -0500

    type added to schedule items, db migration

--- api/models/schedule_item.py
+++ api/models/schedule_item.py
@@ -4,9 +4,19 @@ from api.models import Hackathon
 
 
 class ScheduleItem(models.Model):
+
+    EVENT_TYPE = (
+        (0, 'Key'),
+        (1, 'Tech Talk'),
+        (2, 'Food'),
+        (3, 'Social'),
+        (4, 'Miscellaneous')
+    )
+
     hackathon = models.ForeignKey(to=Hackathon, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500, default='', blank=True)
+    type = models.SmallIntegerField(default=0, choices=EVENT_TYPE)
     start = models.DateTimeField()
     end = models.DateTimeField(null=True, blank=True)
 
@@ -14,8 +24,8 @@ class ScheduleItem(models.Model):
 @admin.register(ScheduleItem)
 class ScheduleItemAdmin(admin.ModelAdmin):
     list_filter = ('hackathon',)
-    list_display = ('hackathon', 'name',  'start', 'end', 'description')
-    list_editable = ('hackathon', 'start', 'end')
+    list_display = ('hackathon', 'type', 'name', 'start', 'end', 'description')
+    list_editable = ('start', 'end')
     list_display_links = ('name',)
     search_fields = ('name',)
     ordering = ('hackathon', 'start')

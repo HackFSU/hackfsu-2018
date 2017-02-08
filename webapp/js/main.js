38a66432f759920f820336f62a7c7394ced084b7
jrdbnntt <jrdbnntt@gmail.com>
2017-02-08 03:50:17 -0500

    schedule on homepage + hackUtil global started

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -5,4 +5,26 @@
 
 (function() {
     'use strict';
+
+    var hackUtil = window.hackUtil = {};
+
+    hackUtil.SCHEDULE_ITEM_TYPES = {
+        0: 'Key',
+        1: 'Tech Talk',
+        2: 'Food',
+        3: 'Social',
+        4: 'Miscellaneous'
+    };
+
+    /**
+     * Date time handling
+     * https://docs.python.org/2/library/datetime.html#datetime.datetime.isoformat -> momentjs format
+     */
+
+    hackUtil.PYTHON_DATETIME_SERIALIZED_ISO_FORMAT = 'YYYY-MM-DDTHH:mm:ss:SSSSSSSSSZ';
+
+    hackUtil.deserializeDateTime = function(pythonIsoDateString) {
+        return moment.utc(pythonIsoDateString, hackUtil.PYTHON_DATETIME_SERIALIZED_ISO_FORMAT).local();
+    };
+
 })();
--- webapp/views/index/script.js
+++ webapp/views/index/script.js
@@ -12,4 +12,77 @@
     });
 
     $('#content-container').parallaxIt();
+
+
+
+
+    /**
+     * Schedule
+     */
+
+    var friSchedule = $('#friSchedule');
+    var satSchedule = $('#satSchedule');
+    var sunSchedule = $('#sunSchedule');
+    var timeFormat = 'h:mma';
+
+    function getScheduleItemIconClass(type) {
+        // Return the font-awesome icon
+        switch (type) {
+            case 0: return 'key';
+            case 1: return 'code';
+            case 2: return 'cutlery';
+            case 3: return 'users';
+            default: return 'calendar-o';
+        }
+    }
+
+    function getScheduleItemTime(item) {
+        var str = item.start.format(timeFormat);
+
+        if (item.end) {
+            str = str.replace(/(am)|(pm)/g, '');
+            str += '-' + item.end.format(timeFormat);
+        }
+
+        return str.replace(/:00/g, '');
+    }
+
+
+    function addScheduleItem(item) {
+        item.start = hackUtil.deserializeDateTime(item.start);
+        if (item.end) {
+            item.end = hackUtil.deserializeDateTime(item.end);
+        }
+        var iconClass = getScheduleItemIconClass(item.type);
+        var e = $(
+            '<div class="schedule-item"><div class="row">' +
+                '<div class="col-xs-8">' +
+                    '<span class="schedule-item-name">'+item.name+'</span>' +
+                    '<span class="fa fa-'+iconClass+'"></span>' +
+                    (item.description? '<div class="schedule-item-description"><span>'+item.description+'</span></div>' : '') +
+                '</div>' +
+                '<div class="col-xs-4 schedule-item-time">' +
+                    '<span>'+getScheduleItemTime(item)+'</span>' +
+                '</div>' +
+            '</div></div>'
+        );
+
+        switch (item.start.format('ddd')) {
+            case 'Fri': friSchedule.append(e); break;
+            case 'Sat': satSchedule.append(e); break;
+            case 'Sun': sunSchedule.append(e); break;
+            default:
+                console.error('Invalid schedule item start date', item);
+        }
+    }
+
+    $.ajaxGet({
+        url: '/api/hackathon/get/schedule_items',
+        success: function(data) {
+            data.schedule_items.forEach(function(item) {
+                addScheduleItem(item);
+            });
+        }
+    });
+
 })();

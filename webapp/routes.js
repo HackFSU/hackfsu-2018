d986d23c20a80611f1df8420b4229462582241d8
Keishon Smith <keishon104@gmail.com>
2017-10-08 20:19:28 -0400

    Create Registration Page
    Implements template and route from #29

--- webapp/routes.js
+++ webapp/routes.js
@@ -11,4 +11,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET registration page. */
+router.get('/registration', function(req, res, next) {
+  res.render('registration', { title: 'registration page' });
+});
+
+
 module.exports = router;

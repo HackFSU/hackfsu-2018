51a740880e1fb497de6750d2268db2a667530e5c
Andrew Sosa <andrewsosa001@gmail.com>
2017-11-04 13:12:54 -0400

    Switch from custom Hack Manager to custom Queryset

--- api/api/models/hack.py
+++ api/api/models/hack.py
@@ -1,7 +1,25 @@
 from django.db import models
-from api.models import Hackathon, JudgingExpo, JudgingCriteria, JudgeInfo
+from api.models import Hackathon
+from api.models.judging_criteria import JudgingCriteria
+from api.models.judging_expo import JudgingExpo
 from django.contrib import admin
 from hackfsu_com.admin import hackfsu_admin
+
+
+class HackQuerySet(models.QuerySet):
+    from api.models.judge_info import JudgeInfo
+
+    def from_expo(self, expo: JudgingExpo):
+        return self.filter(
+            table_number__gte=expo.table_number_start,
+            table_number__lte=expo.table_number_end
+        )
+
+    def with_active_judge(self, judge: JudgeInfo):
+        return self.filter(current_judges=judge)
+
+    def without_previous_judge(self, judge: JudgeInfo):
+        return self.exclude(judges=judge)
 
 
 class HackManager(models.Manager):
@@ -14,7 +32,7 @@ class HackManager(models.Manager):
 
 
 class Hack(models.Model):
-    objects = HackManager()
+    objects = HackManager.from_queryset(HackQuerySet)()
     hackathon = models.ForeignKey(to=Hackathon, on_delete=models.CASCADE)
     table_number = models.IntegerField()
     name = models.CharField(max_length=100)                                             # Devpost "Submission Title"
@@ -24,10 +42,6 @@ class Hack(models.Model):
     judges = models.ManyToManyField(to='api.JudgeInfo', blank=True, related_name='judges')
     total_judge_score = models.IntegerField(default=0)
     times_judged = models.IntegerField(default=0)
-
-    @staticmethod
-    def get_active_hacks_for_judge(judge):
-        return Hack.objects.filter(current_judges=judge)
 
     def get_expo(self):
         expo = JudgingExpo.objects.filter(

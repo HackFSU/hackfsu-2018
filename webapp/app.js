ed8dd2f5ff17a381ade740e045aa762e54d73e2e
Andrew Sosa <andrewsosa001@gmail.com>
2018-01-18 07:10:50 -0500

    Restructure route controllers, add eslint

--- webapp/app.js
+++ webapp/app.js
@@ -1,53 +1,49 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var sassMiddleware = require('node-sass-middleware');
-var routes = require('./routes');
+const express           = require('express');
+const bodyParser        = require('body-parser');
+const cookieParser      = require('cookie-parser');
+const favicon           = require('serve-favicon');
+const logger            = require('morgan');
+const sassMiddleware    = require('node-sass-middleware');
+const path              = require('path');
 
 
-var app = express();
+const routes = require('./routes');
+const app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
+//
+//  Middleware
+//
+
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public/sass'),
-  dest: path.join(__dirname, 'public/css'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true,
-  debug: true,
-  prefix: '/css'
+    src: path.join(__dirname, 'public/sass'),
+    dest: path.join(__dirname, 'public/css'),
+    indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true,
+    debug: true,
+    prefix: '/css'
 }));
+
+//
+//  Routing + Helpers
+//
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', require('./controllers'));
 
-app.use('/', routes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+//
+//  Startup
+//
+
+app.listen(3000, () => {
+    console.log('Express Webapp listening on port 3000');
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
---
+++ webapp/controllers/index.js
@@ -0,0 +1,17 @@
+const express = require('express'),
+    router = express.Router();
+
+//  Index page
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'HackFSU 5'
+    });
+});
+
+//  Other routes
+// router.use('/register', require('./register'));
+
+//  Error Handling
+router.use(require('./errors'));
+
+module.exports = router;

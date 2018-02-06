680e01db0ad06088b3809af9c2619dea1803c6bb
Andrew Sosa <andrewsosa001@gmail.com>
2018-02-06 15:46:50 -0500

    Update webapp login logic

--- webapp/controllers/profile.js
+++ webapp/controllers/profile.js
@@ -4,46 +4,88 @@
 //
 
 const bodyParser    = require('body-parser');
+const cookieParser  = require('cookie-parser');
 const express       = require('express');
 const request       = require('request');
 
-const router = express.Router();
+//
+//  Three routers:
+//      authRouter:     Primary router handling auth-related paths.
+//      loginRouter:    Router for logging in and out.
+//      profileRouter:  These routes require user to be logged in.
+
+const authRouter = express.Router();
+const loginRouter = express.Router();
+const profileRouter = express.Router();
+
+authRouter.use(bodyParser.urlencoded({ extended: true }));
+authRouter.use(bodyParser.json());
+authRouter.use(cookieParser());
+authRouter.use('/profile', profileRouter);
+authRouter.use(loginRouter);
+
+profileRouter.use(require('../middleware/auth'));
+
 
 //
 //  Login
 
-router.get('/login', (req, res) => {
+loginRouter.get('/login', (req, res) => {
+    let msg = decodeURIComponent(req.query.msg);
+    console.log('msg', msg);
 
     res.render('profile/login.pug', {
-        title: 'HackFSU 5 | Log In'
+        title: 'HackFSU 5 | Log In',
+        msg: msg
     });
-
 });
 
-router.post('/login', (req, res) => {
+loginRouter.post('/login', (req, res) => {
     let host = req.app.get('api-host');
 
-    req.pipe(request.post(host + '/api/user/login'))
-        .on('response', (resp) => {
-            // let Location = '/';
-            res.set(resp.headers);
-            res.redirect('/profile');
+    // console.log(req.body.email);
+    let email = req.body.email,
+        password = req.body.password;
+
+    request.post(host + '/api/user/login',
+        { json: { email, password }},
+        (err, resp, body) => {
+
+            // Successful login
+            if (resp.statusCode === 200) {
+                res.set(resp.headers);
+                res.redirect('/profile');
+            }
+
+            // Bad login
+            else {
+                let msg = encodeURIComponent('Incorrect username or password.');
+                res.redirect(`/login?msg=${msg}`);
+            }
         });
 
 });
 
+loginRouter.get('/logout', (req, res) => {
+    let host = req.app.get('api-host');
+    res.clearCookie('sessionid');
+    res.clearCookie('csrftoken');
+    res.redirect('/');
+});
 
 //
 // Profile
 
-router.get('/profile', (req, res) => {
-    let host = req.app.get('api-host');
+profileRouter.get('/', (req, res) => {
 
-    req.pipe(request.get(host + '/api/user/get/profile', {}, (err, resp, body) => {
-        res.send(body);
-    }));
+    res.json(JSON.parse(req.profile));
+    // let host = req.app.get('api-host');
+
+    // req.pipe(request.get(host + '/api/user/get/profile', {}, (err, resp, body) => {
+    //     res.send(body);
+    // }));
 
 });
 
 
-module.exports = router;
+module.exports = authRouter;
